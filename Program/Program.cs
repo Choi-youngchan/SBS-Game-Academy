d75d7d59ed40da4971e0b617bf3a06ebd67c8ccd
Choi-youngchan <dudcks5050@gmail.com>
2024-05-20 15:42:14 +0900

    2024.5.20 배열

--- Program/Program.cs
+++ Program/Program.cs
@@ -4,11 +4,9 @@ namespace Program
 {
     internal class Program
     {
-        // Main 함수는 프로그램의 진입점 역할을 수행하는 함수입니다.
+
         static void Main(string[] args)
         {
         }
-    }
-
-    }
+    }    
 }
