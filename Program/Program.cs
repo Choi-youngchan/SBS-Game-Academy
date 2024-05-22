48d6769324f1db41e69ff8fc68ec2bb5bd9f6ff3
Choi-youngchan <dudcks5050@gmail.com>
2024-05-22 15:44:41 +0900

    2024.05.22 클래스

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.IO;
+using System.Xml.Linq;
 
 namespace Program
 {
+
     internal class Program
     {
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!")
+
         }
     }    
 }
